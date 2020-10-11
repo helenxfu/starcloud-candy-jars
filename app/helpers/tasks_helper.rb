5a2f30ae9afc18abe976a191e096ae9f71d187a3
helenxfu <helenxfu@gmail.com>
2020-10-11 23:14:03 +0900

    add hadle deadline helper

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
 module TasksHelper
+
+  def handle_deadline(deadline)
+    day = (deadline - Date.today).to_i
+    if day > 0
+      return pluralize(day, "day")
+    elsif day == 0
+      return "TODAY"
+    else
+      return "OVERDUE"
+    end
+  end
 end
