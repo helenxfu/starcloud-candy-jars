c5c8b604e6c9fec22aa5326678400974b8e26e85
helenxfu <helenxfu@gmail.com>
2020-10-11 22:39:34 +0900

    hide chatroom and handle non-logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
       get :delete_all, :delete_completed
     end
   end
-  get "chatroom", to: "chatroom#index"
-  resources :messages, only: [:create, :destroy]
+  # get "chatroom", to: "chatroom#index"
+  # resources :messages, only: [:create, :destroy]
   root "pages#home"
   get "about", to: "pages#about"
   resources :users, except: [:new]
