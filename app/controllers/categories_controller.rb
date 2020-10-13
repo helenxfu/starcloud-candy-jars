e57aa503e2a24f2a2cd4cc32c38d928082ad868e
helenxfu <helenxfu@gmail.com>
2020-10-13 21:23:49 +0900

    reorganize to standard view for category and task

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,36 +1,27 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
-  # GET /categories
-  # GET /categories.json
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 20)
+    @categories = current_user.categories.paginate(page: params[:page], per_page: 20)
   end
 
-  # GET /categories/1
-  # GET /categories/1.json
   def show
     @tasks = @category.tasks.paginate(page: params[:page], per_page: 20)
   end
 
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
   def edit
   end
 
-  # POST /categories
-  # POST /categories.json
   def create
     @category = current_user.categories.new(category_params)
 
     if @category.save
       flash[:success] = "category created!"
-      redirect_to tasks_path
-      # redirect_back(fallback_location: root_url)
+      redirect_to categories_path
     else
       flash[:danger] = "category create failed: #{@category.errors.full_messages}"
       redirect_back(fallback_location: root_url)
@@ -42,7 +33,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to tasks_path, notice: "Category was successfully updated." }
+        format.html { redirect_to categories_path, notice: "Category was successfully updated." }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit }
@@ -56,7 +47,7 @@ class CategoriesController < ApplicationController
   def destroy
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_path, notice: "Category was successfully destroyed." }
+      format.html { redirect_to categories_path, notice: "Category was successfully destroyed." }
       format.json { head :no_content }
     end
   end
