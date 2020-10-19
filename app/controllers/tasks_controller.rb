43efc10194103e9f58a3819ecd8fa99534b9e069
helenxfu <helenxfu@gmail.com>
2020-10-19 21:16:06 +0900

    individually working form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,25 @@
 class TasksController < ApplicationController
   before_action :require_user
   before_action :set_task, only: [:edit, :update, :destroy]
+  before_action :index_params, only: :index
 
   def index
-    @tasks = current_user.tasks.paginate(page: params[:page], per_page: 50)
+    # debugger
+    if params[:search]
+      @tasks = current_user.tasks.where("name LIKE ?", "%#{params[:search]}%").paginate(page: params[:page], per_page: 50)
+    elsif params[:category_id]
+      category_id = params[:category_id].to_i
+      @tasks = current_user.categories.find(category_id).tasks.paginate(page: params[:page], per_page: 50)
+    elsif params[:completion]
+      completion = params[:completion] == "true" ? true : false
+      @tasks = current_user.tasks.where(completed: completion).paginate(page: params[:page], per_page: 50)
+    elsif params[:priority]
+      priority = params[:priority].to_i
+      @tasks = current_user.tasks.where(priority: priority).paginate(page: params[:page], per_page: 50)
+    else
+      @tasks = current_user.tasks.paginate(page: params[:page], per_page: 50)
+    end
   end
-
 
   def new
     @task = Task.new
@@ -55,6 +69,11 @@ class TasksController < ApplicationController
     redirect_back(fallback_location: root_url)
   end
 
+  def filter_tasks(id)
+    @tasks = current_user.tasks.where(category: id).paginate(page: params[:page], per_page: 50)
+    render tasks
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
@@ -65,5 +84,9 @@ class TasksController < ApplicationController
   # Only allow a list of trusted parameters through.
   def task_params
     params.require(:task).permit(:name, :priority, :completed, :deadline, category_ids: [])
+  end
+
+  def index_params
+    # params.permit(:search, :category_id, :completion)
   end
 end
